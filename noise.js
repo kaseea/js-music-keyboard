4ac5f335bdf8548600fc10e6e4d952149e29a5bc
K <774162+kaseea@users.noreply.github.com>
2019-05-28 20:18:45 -0700

    whelp it works

--- noise.js
+++ noise.js
@@ -1,3 +1,28 @@
 $(document).ready( function() {
-  // your code here
+   
+    $('body').on('click', 'button', function(event) {
+      let noteLetter = `${$(this).html()}`;
+      let classy = noteLetter + `Audio`;
+      let mediaElem = document.getElementById(classy);
+      mediaElem.load();
+      mediaElem.play();   
+    });
+
+    $('body').keydown((e) => {
+      // console.log(e.key);
+      let classy = e.key + `Audio`;
+      // console.log(classy)
+      let mediaElem2 = document.getElementById(classy);
+      // console.log(mediaElem2);
+      mediaElem2.load();
+      mediaElem2.play();
+    })
+        // this is amazing and all you need, gives you the letter
+        // console.log(`${$(this).html()}`)
+  
 });
+
+
+
+// var mediaElem = document.getElementById("my-media-element");
+// mediaElem.load();
